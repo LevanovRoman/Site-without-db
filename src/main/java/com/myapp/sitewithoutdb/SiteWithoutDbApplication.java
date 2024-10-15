3c13ae556983e2e298f9f7e5075f8c22526ec9ca
levanovroman <levanovroman2016@yandex.ru>
2024-10-15 12:08:17 +0300

    first commit

---
+++ src/main/java/com/myapp/sitewithoutdb/SiteWithoutDbApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.sitewithoutdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteWithoutDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteWithoutDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/sitewithoutdb/SiteWithoutDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.sitewithoutdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteWithoutDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
